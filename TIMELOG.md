d3315be260a16fd10913d6a297e089b41c941784
Tor Nilsson Ohrn <tornilssonohrn@gmail.com>
2014-09-12 11:13:40 +0200

    colored barcode style week with all entries

---
+++ TIMELOG.md
@@ -0,0 +1,4 @@
+## Timelog for lifelab
+* 2014-09-11 dirty print of all entries in one week 1 hrs 20 min
+
+### Total time spent: 1 hours and 20 minutes 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,43 @@
 
 var timelogApp = angular.module('timelogApp', []);
 
+var categories;
+
 timelogApp.controller('EntryListCtrl', function ($scope, $http) {
-	$http.get('data/week1.json')
-       .then(function(res){
-          $scope.entries = res.data;  
-          var totalTime = getTotalTime($scope.entries);
-          $scope.totalHrs=totalTime.hrs;
-          $scope.totalMin=totalTime.min;
-        });
+	$http.get('data/week1.json').then(function(res){
+    addEntries(res.data);  
+  });
+  // calculate total minutes and hours for some given entries
+	// -----------------------
+	function addEntries(entries) {
+		categories = [];
+		var total = 0;
+		for (var i in entries) {
+			var cur = entries[i];
+		// convert minutes and seconds to decimal hours for simpler calculations
+		var parts = cur.Duration.split(':');
+		DecDuration = parts[0]/2.4+parts[1]/144;
+		cur.DecDuration=DecDuration;
+		// sum up decimal hours
+		total+=DecDuration;
+		// add category of entry to know categorys if it is a new one
+		if (categories.indexOf(cur.Task) < 0) {
+			categories.push(cur.Task);
+		}
+	};
+	$scope.entries = entries;
+	$scope.totalDecimalTime  = total;
+	$scope.categories = categories;
+	}
+	// calculate entries opacity
+	$scope.getEntryColor = function (entry) {
+		var shade = categories.indexOf(entry.Task)/categories.length;
+		return shade;
+	}
 
 });
 
-// calculate total minutes and hours for some given entries
-// -----------------------
-
+// total time in old school base 60 format
 function getTotalTime(entries) {
 	var total =[];
 	total.min = 0;
@@ -26,6 +49,7 @@ function getTotalTime(entries) {
 	}
 	total.hrs+=total.min/60
 	total.min=total.min%60
-	return total
+	return total;
 }
+
 
